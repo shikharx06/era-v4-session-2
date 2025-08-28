25bd9ed6efc327580957e79e21818e7f0f05cdae
root <root@e2e-78-189.ssdcloudindia.net>
2025-08-28 22:51:10 +0530

    chore:added certs

--- fastapi-image-server/main.py
+++ fastapi-image-server/main.py
@@ -278,4 +278,4 @@ async def serve_image(image_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=4444, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4444, reload=True, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
